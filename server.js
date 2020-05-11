4b7652930b4cb7b79f8e509c1bb4e340249c25ed
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-05-11 09:35:13 -0300

    criando api

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const body = require('body-parser');
-const express = require('express');
-   
-const app1 = express();
-const app2 = express();
+require('dotenv').config()
 
-app1.use(body.json());
-app2.use(body.json());
-   
-const handler = serverNum => (req, res) => {
- res.send(`hello`);
-};
+const express = require('express')   
+const cors = require('cors')
+const app = express()
 
-app1.get('/hello2', handler(1))
-app2.get('/hello2', handler(2))
+app.use(express.json())
+app.use(cors())
+app.disable('x-powered-by')
+
+app.get('/hello', (req, res) => {
+    res.send(`hello`)
+})
                  
-app1.listen(3000);
-app2.listen(3001);
+app.listen(process.env.PORT || 3000)
